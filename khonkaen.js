08597b60f018e8861e5dbe43b787f831a8154248
Faknp <125230855+Faknp@users.noreply.github.com>
2023-04-24 11:29:02 +0700

    Add files via upload

---
+++ khonkaen.js
@@ -0,0 +1,155 @@
+var tourkhonkaen = {
+    "type": "FeatureCollection",
+    "features": [
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "วัดทุ่งเศรษฐี",
+          "type": "Culture",
+          "level": 8
+        },
+        "geometry": {
+          "coordinates": [
+            102.88370527116615,
+            16.411516441845137
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "บึงแก่นนคร",
+          "type": "Natural",
+          "level": 10
+        },
+        "geometry": {
+          "coordinates": [
+            102.83521399999893,
+            16.416319233355537
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "วัดหนองแวง",
+          "type": "Culture",
+          "level": 8
+        },
+        "geometry": {
+          "coordinates": [
+            102.83221599999888,
+            16.457123231627037
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "โฮงมูนมัง",
+          "type": " Museum",
+          "level": 9
+        },
+        "geometry": {
+          "coordinates": [
+            102.839145999999,
+            16.419504733220975
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "สวนสัตว์เขาสวนกวาง",
+          "type": "Natural",
+          "level": 10
+        },
+        "geometry": {
+          "coordinates": [
+            102.89660212037774,
+            16.846365253467567
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "หอศิลปวัฒนธรรม ม.ขอนแก่น",
+          "type": " Museum",
+          "level": 9
+        },
+        "geometry": {
+          "coordinates": [
+            102.81462600000015,
+            16.445506942205014
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "พิพิธภัณฑ์ประวัติศาสตร์ธรรมชาติ ม.ขอนแก่น",
+          "type": " Museum",
+          "level": 7
+        },
+        "geometry": {
+          "coordinates": [
+            102.81091382272291,
+            16.445813581564764
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "เขื่อนอุบลรัตน์",
+          "type": "Natural",
+          "level": 9
+        },
+        "geometry": {
+          "coordinates": [
+            102.62648999999936,
+            16.739349219548217
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "เซ็นทรัลพลาซ่าขอนแก่น",
+          "type": "Department store",
+          "level": 8
+        },
+        "geometry": {
+          "coordinates": [
+            102.82539645539129,
+            16.432564388144655
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "ตลาดต้นตาลขอนแก่น",
+          "type": "Market",
+          "level": 8
+        },
+        "geometry": {
+          "coordinates": [
+            102.81848927116351,
+            16.418298941917087
+          ],
+          "type": "Point"
+        }
+      }
+    ]
+  }
